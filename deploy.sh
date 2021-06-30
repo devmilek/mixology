17cb248f5404f6e98775000cebc2b73fc9811dfe
Miłosz Kraiński <61127609+lilmilek@users.noreply.github.com>
2021-06-30 18:51:20 +0200

    detail page n ev

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+##!/usr/bin/env sh
+#
+#set -e
+#
+#npm run build
+#
+#cd dist
+#
+#git init
+#git add -A
+#git commit -m 'New deploy'
+#git push -f git@github.com:lilmilek/mixology.git master:gh-pages
+#
+#cd -
